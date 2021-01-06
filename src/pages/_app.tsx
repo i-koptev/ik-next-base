e5d703b817457c1cab3d4a0a407e3ee524be5ddb
Igor Koptev <igor.koptef@gmail.com>
2021-01-06 08:35:47 +0200

    Lang switcher moved to Main layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,15 @@
 import React from "react"
 import Head from "next/head"
 import { AppProps } from "next/app"
-import Container from "@material-ui/core/Container"
 
 import { ThemeProvider } from "@material-ui/core/styles"
 import CssBaseline from "@material-ui/core/CssBaseline"
-import theme from "../../src/theme"
+import theme from "../theme/index"
 
-import { useRouter } from "next/router"
-// import Link from "next/link"
-import Link from "../components/Link"
-
-import "../styles/globals.css"
+// import "../styles/globals.css"
 
 export default function MyApp(props: AppProps) {
     const { Component, pageProps } = props
-    const router = useRouter()
 
     React.useEffect(() => {
         // Remove the server-side injected CSS.
@@ -37,18 +31,7 @@ export default function MyApp(props: AppProps) {
             <ThemeProvider theme={theme}>
                 {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
                 <CssBaseline />
-                <Container maxWidth="lg">
-                    <ul>
-                        {router.locales.map((locale) => (
-                            <li key={locale}>
-                                <Link href={router.asPath} locale={locale}>
-                                    {locale}&rarr;
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                    <Component {...pageProps} />
-                </Container>
+                <Component {...pageProps} />
             </ThemeProvider>
         </React.Fragment>
     )
