848fcd2e270736a892231891548efacd455791fc
Igor Koptev <igor.koptef@gmail.com>
2021-01-06 09:07:18 +0200

    Dumb pages added

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import MainLayout from "../layouts/Main"
+
+const About = () => (
+    <MainLayout>
+        <div>About</div>
+    </MainLayout>
+)
+
+export default About
---
+++ src/pages/contacts.js
@@ -0,0 +1,9 @@
+import MainLayout from "../layouts/Main"
+
+const Contacts = () => (
+    <MainLayout>
+        <div>Contacts</div>
+    </MainLayout>
+)
+
+export default Contacts
