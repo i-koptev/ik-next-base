c01a1f9873b43e6e525a44068237a0c470981486
Igor Koptev <igor.koptef@gmail.com>
2021-01-06 08:22:35 +0200

    Added Main layout

---
+++ src/layouts/Main/Main.tsx
@@ -0,0 +1,176 @@
+import React, { useState } from "react"
+import clsx from "clsx"
+import { makeStyles, useTheme } from "@material-ui/styles"
+import { useMediaQuery } from "@material-ui/core"
+import Container from "@material-ui/core/Container"
+
+import useScrollTrigger from "@material-ui/core/useScrollTrigger"
+import Fab from "@material-ui/core/Fab"
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp"
+import Zoom from "@material-ui/core/Zoom"
+
+// import { Sidebar, Sidebar2, Topbar, Footer } from "./components"
+import { TopbarHeadroom } from "./components"
+// import { Sidebar, TopbarHeadroom, Footer } from "./components"
+// import SEO from "./Seo" TODO
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        // paddingTop: 56,
+        [theme.breakpoints.up("sm")]: {
+            // paddingTop: 64,
+        },
+    },
+    rootHeight: {
+        height: "100%",
+    },
+    shiftContent: {
+        paddingLeft: 240,
+    },
+    content: {
+        height: "100%",
+    },
+    fab: {
+        width: "48px",
+        height: "48px",
+        [theme.breakpoints.up("sm")]: {
+            width: "56px",
+            height: "56px",
+        },
+        opacity: 0.8,
+
+        color: "#777",
+        "&:hover": {
+            opacity: 1,
+            color: "black",
+        },
+    },
+}))
+
+function ScrollTop(props) {
+    const { children, window } = props
+    const classes = useStyles()
+    // Material UI docs:
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+        // target: window ? window() : undefined,
+        disableHysteresis: true,
+        threshold: 100,
+    })
+
+    const handleClick = (event) => {
+        const anchor = (event.target.ownerDocument || document).querySelector(
+            "#back-to-top-anchor"
+        )
+
+        if (anchor) {
+            anchor.scrollIntoView({ behavior: "smooth", block: "center" })
+        }
+    }
+
+    return (
+        <Zoom in={trigger} timeout={500}>
+            <div
+                onClick={handleClick}
+                role="presentation"
+                style={{
+                    position: "fixed",
+                    bottom: "30px",
+                    right: "30px",
+                }}
+            >
+                {children}
+            </div>
+        </Zoom>
+    )
+}
+
+const Main = (props) => {
+    const { children, lang } = props
+
+    const classes = useStyles()
+    const theme = useTheme()
+    const isDesktop = useMediaQuery(theme.breakpoints.up("lg"), {
+        defaultMatches: true,
+    })
+
+    const [openSidebar, setOpenSidebar] = useState(false)
+
+    const handleSidebarOpen = () => {
+        setOpenSidebar(true)
+    }
+
+    const handleSidebarClose = () => {
+        setOpenSidebar(false)
+    }
+
+    // const shouldOpenSidebar = isDesktop ? true : openSidebar //comment to show sidebar on Desktop
+    const shouldOpenSidebar = openSidebar //uncomment to show sidebar on Desktop
+
+    return (
+        <div
+            className={clsx(
+                {
+                    // [classes.shiftContent]: isDesktop, //uncomment to show sidebar on Desktop
+                },
+                classes.rootHeight
+            )}
+        >
+            <div id="back-to-top-anchor" />
+            <TopbarHeadroom onSidebarOpen={handleSidebarOpen} />
+            {/* <Sidebar
+                onClose={handleSidebarClose}
+                open={shouldOpenSidebar}
+                variant={isDesktop ? "persistent" : "temporary"}
+                lang={lang}
+            /> */}
+            {/* <Container
+                maxWidth={theme.siteContainer.maxWidth}
+                component="main"
+                className={classes.content}
+            > */}
+            {children}
+
+            {/* </Container> */}
+            {/* <Footer /> */}
+            <ScrollTop {...props}>
+                <Fab
+                    className={classes.fab}
+                    size="small"
+                    aria-label="scroll back to top"
+                >
+                    <KeyboardArrowUpIcon />
+                </Fab>
+            </ScrollTop>
+        </div>
+    )
+}
+
+export default Main
+
+// import React from "react"
+// import Footer from "./Footer"
+// import Navigation from "./Navigation"
+// import SEO from "./Seo"
+
+// const Layout = ({ title, description, lang, children }) => {
+//     return (
+//         <div
+//             style={{
+//                 backgroundImage: "url(/img/bg_16.jpg)",
+//                 backgroundRepeat: "repeat",
+//                 minHeight: "100vh",
+//                 height: "100%",
+//             }}
+//         >
+//             <SEO title={title} description={description} lang={lang} />
+//             <Navigation />
+//             {children}
+//             <Footer />
+//         </div>
+//     )
+// }
+
+// export default Layout
---
+++ src/layouts/Main/components/TopbarHeadroom/TopbarHeadroom.tsx
@@ -0,0 +1,247 @@
+import React, { useState } from "react"
+import { useRouter } from "next/router"
+import useTranslation from "next-translate/useTranslation"
+import Headroom from "react-headroom"
+import clsx from "clsx"
+
+import { makeStyles, useTheme } from "@material-ui/styles"
+import { AppBar, Toolbar, Badge, Hidden, IconButton } from "@material-ui/core"
+import Container from "@material-ui/core/Container"
+import Button from "@material-ui/core/Button"
+import MenuIcon from "@material-ui/icons/Menu"
+import NotificationsIcon from "@material-ui/icons/NotificationsOutlined"
+import InputIcon from "@material-ui/icons/Input"
+
+import Link from "../../../../components/Link"
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        // backgroundColor: theme.layouts.Main.Topbar.backgroundColor,
+        backgroundColor: "transparent", // needs to be transparent for ReactHeadroom colors to work
+        boxShadow: theme.layouts.Main.Topbar.boxShadow,
+    },
+    logo: {
+        color: theme.layouts.Main.Topbar.logoColor,
+        textDecoration: "none !important",
+        fontSize: "1rem",
+        fontWeight: 700,
+        textTransform: "uppercase",
+        letterSpacing: "0.1rem",
+    },
+    flexGrow: {
+        flexGrow: 1,
+    },
+    signOutButton: {
+        marginLeft: theme.spacing(1),
+    },
+    mainNavigationLink: {
+        transition: "color 200ms linear",
+        textDecoration: "none",
+        // textTransform: "uppercase",
+        textTransform: "capitalize",
+        marginRight: "1rem",
+        color: theme.layouts.Main.Topbar.mainNavigationLinkColor,
+        fontWeight: 400,
+        letterSpacing: "0.15em",
+        "&::after": {
+            content: '""',
+            width: "100%",
+            height: "1px",
+            margin: "0 auto",
+            backgroundColor:
+                theme.layouts.Main.Topbar.mainNavigationLinkActiveColor,
+            display: "block",
+            // margin-bottom: .3rem;
+            opacity: "0",
+            transitionDuration: "500ms",
+            transitionProperty: "opacity",
+        },
+        "&:hover": {
+            color: theme.layouts.Main.Topbar.mainNavigationLinkHoverColor,
+            "&::after": {
+                opacity: "0.5",
+            },
+        },
+        "&$active": {
+            color: theme.layouts.Main.Topbar.mainNavigationLinkActiveColor,
+            "&:hover": {
+                cursor: "default",
+                // color:
+                //     theme.layouts.Main.Topbar
+                //         .mainNavigationLinkActiveHoverColor,
+                "&::after": {
+                    opacity: "1",
+                },
+            },
+            "&::after": {
+                content: '""',
+                width: "100%",
+                height: "1px",
+                margin: "0 auto",
+                backgroundColor:
+                    theme.layouts.Main.Topbar.mainNavigationLinkActiveColor,
+                display: "block",
+                // margin-bottom: .3rem;
+                opacity: "1",
+                transitionDuration: "500ms",
+                transitionProperty: "opacity",
+            },
+        },
+    },
+    active: {},
+    langSwitcherButton: {
+        color: theme.layouts.Main.Topbar.langSwitcherButtonColor,
+        "&:hover": {
+            // backgroundColor: "transparent",
+            outline: `2px solid ${theme.layouts.Main.Topbar.langSwitcherButtonHoverOutlineColor}`,
+            color: theme.layouts.Main.Topbar.langSwitcherButtonHoverColor,
+        },
+        "& .MuiButton-label": {
+            letterSpacing: "0.02rem",
+        },
+    },
+    toolbar: {
+        minHeight: "56px",
+        [theme.breakpoints.up("sm")]: {
+            minHeight: "64px",
+        },
+    },
+    headroom: {
+        "& .headroom": {
+            transition:
+                "background-color 500ms linear, border-bottom 500ms linear",
+            backgroundColor: theme.layouts.Main.Topbar.backgroundColor,
+            borderBottom: theme.mainNavigationBorderBottom,
+            position: "fixed",
+            width: "100%",
+        },
+        "& .headroom--pinned": {
+            backgroundColor: theme.layouts.Main.Topbar.pinnedBackgroundColor,
+            borderBottom: theme.mainNavigationPinnedBorderBottom,
+        },
+    },
+    burger: {
+        color: theme.layouts.Main.Topbar.burgerColor,
+        marginRight: "0.9rem",
+    },
+}))
+
+const TopbarHeadroom = (props) => {
+    const { className, onSidebarOpen, ...rest } = props
+
+    const classes = useStyles()
+    const theme = useTheme()
+    const router = useRouter()
+    const menuItems = [{ slug: "/contacts" }, { slug: "/about" }]
+    // const [notifications] = useState([])
+
+    const [unfixed, setUnfixed] = useState()
+    const { t, lang } = useTranslation("common")
+
+    return (
+        <Headroom
+            onPin={() => console.log("pinned")}
+            onUnpin={() => console.log("unpinned")}
+            onUnfix={() => console.log("unfixed")}
+            wrapperStyle={{ height: "100px" }}
+            variant="header"
+            className={classes.headroom}
+        >
+            <AppBar
+                {...rest}
+                className={clsx(classes.root, className)}
+                position="static"
+            >
+                <Container maxWidth={theme.siteContainer.maxWidth}>
+                    <Toolbar
+                        component="nav"
+                        disableGutters
+                        classes={{ root: classes.toolbar }}
+                    >
+                        <Link href="/" className={classes.logo}>
+                            {/* <img alt="Logo" src="/images/logos/logo--white.svg" /> */}
+                            IK-Base
+                        </Link>
+                        <div className={classes.flexGrow} />
+                        <Hidden mdDown>
+                            {menuItems.slice(0, 4).map((menuItem) => (
+                                <Link
+                                    key={`key-${menuItem.slug}`}
+                                    naked
+                                    className={classes.mainNavigationLink}
+                                    // activeClassName={classes.active}
+                                    href={
+                                        menuItem.slug === "/"
+                                            ? menuItem.slug
+                                            : `/${menuItem.slug.replace(
+                                                  /\//g,
+                                                  ""
+                                              )}/`
+                                    }
+                                >
+                                    {t(
+                                        `topbarMenu-${menuItem.slug.replace(
+                                            /\//g,
+                                            ""
+                                        )}-title`
+                                    )}
+                                </Link>
+                            ))}
+                        </Hidden>
+
+                        {router.locales
+                            .filter((locale) => locale !== lang)
+                            .map((locale) => (
+                                <Button
+                                    key={locale}
+                                    className={classes.langSwitcherButton}
+                                    href={router.asPath}
+                                    locale={locale}
+                                    component={Link}
+                                >
+                                    {locale.toUpperCase()}
+                                </Button>
+                            ))}
+
+                        <Hidden mdDown>
+                            <IconButton color="inherit">
+                                <Badge
+                                    // badgeContent={notifications.length}
+                                    badgeContent={7}
+                                    // color="primary"
+                                    color="error"
+                                    // variant="dot"
+                                >
+                                    <NotificationsIcon />
+                                </Badge>
+                            </IconButton>
+                            <IconButton
+                                className={classes.signOutButton}
+                                color="inherit"
+                            >
+                                <InputIcon />
+                            </IconButton>
+                        </Hidden>
+                        <Hidden lgUp>
+                            <IconButton
+                                style={{ paddingRight: 0 }}
+                                color="inherit"
+                                onClick={onSidebarOpen}
+                            >
+                                <MenuIcon className={classes.burger} />
+                            </IconButton>
+                        </Hidden>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </Headroom>
+    )
+}
+
+// TopbarHeadroom.propTypes = {
+//     className: PropTypes.string,
+//     onSidebarOpen: PropTypes.func,
+// }
+
+// export default injectIntl(TopbarHeadroom)
+export default TopbarHeadroom
