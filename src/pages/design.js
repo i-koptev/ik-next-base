8f72f454cfdd5b983643cd4ccab87e3023a0c500
Igor Koptev <igor.koptef@gmail.com>
2021-01-13 10:54:21 +0200

    Test pages...

---
+++ src/pages/design.js
@@ -0,0 +1,9 @@
+import MainLayout from "../layouts/Main"
+
+const Design = () => (
+    <MainLayout>
+        <div>Design</div>
+    </MainLayout>
+)
+
+export default Design
---
+++ src/pages/development.js
@@ -0,0 +1,9 @@
+import MainLayout from "../layouts/Main"
+
+const Development = () => (
+    <MainLayout>
+        <div>Development</div>
+    </MainLayout>
+)
+
+export default Development
