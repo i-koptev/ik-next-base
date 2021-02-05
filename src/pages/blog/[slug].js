cab4261f1bb1eee1e12040a2f1c55599c4c6f2e1
Igor Koptev <igor.koptef@gmail.com>
2021-02-05 16:38:34 +0200

    Added fallback with the spinner to the blog

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -1,11 +1,47 @@
 import Head from "next/head"
 import { useRouter } from "next/router"
 
+import { makeStyles, useTheme } from "@material-ui/styles"
+import CircularProgress from "@material-ui/core/CircularProgress"
+
 import MainLayout from "../../layouts/Main"
 
 import { getAllPostSlugs, getPostData } from "../../lib/posts"
 
-const Post = ({ postData }) => {
+const useStyles = makeStyles((theme) => ({
+    spinner: {
+        backgroundColor: "#000",
+        color: theme.palette.primary.main,
+        fontSize: "3rem",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+}))
+
+const Post = (props) => {
+    const { postData, iserr } = props
+
+    const router = useRouter()
+    const classes = useStyles()
+    const theme = useTheme()
+
+    if (router.isFallback) {
+        return (
+            <div className={classes.spinner}>
+                <CircularProgress size={100} thickness={1} />
+            </div>
+        )
+    }
+
+    if (iserr) {
+        return (
+            <div className={classes.spinner}>
+                <h1>Shit happens - no page found!</h1>
+            </div>
+        )
+    }
     return (
         <MainLayout>
             <Head>
@@ -15,7 +51,7 @@ const Post = ({ postData }) => {
             <p>{postData.date}</p>
             <p>{postData.slug}</p>
             <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-            {/* <pre>{JSON.stringify(postData, null, 2)}</pre> */}
+            <pre>{JSON.stringify(postData, null, 2)}</pre>
         </MainLayout>
     )
 }
@@ -26,7 +62,8 @@ export async function getStaticPaths({ locales, defaultLocale }) {
     const paths = getAllPostSlugs(locales, defaultLocale)
     return {
         paths,
-        fallback: false,
+        // fallback: false,
+        fallback: true,
     }
 }
 
@@ -36,10 +73,21 @@ export async function getStaticProps({
     locales,
     defaultLocale,
 }) {
-    const postData = await getPostData(params.slug, locale, defaultLocale)
-    return {
-        props: {
-            postData,
-        },
+    try {
+        const postData = await getPostData(params.slug, locale, defaultLocale)
+        return {
+            props: {
+                postData,
+            },
+        }
+    } catch (err) {
+        console.log(err)
+        const postData = []
+        return {
+            props: {
+                postData,
+                iserr: true,
+            },
+        }
     }
 }
